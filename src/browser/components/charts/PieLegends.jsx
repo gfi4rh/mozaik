31bc82ba62fc81803318fd2fbe3e0ac930f49d9d
colin27091 <colinregourd@gmail.com>
2021-01-14 14:29:24 +0100

    restore charts

---
+++ src/browser/components/charts/PieLegends.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+
+
+class PieLegends extends Component {
+    render() {
+        const { legends } = this.props;
+
+        const legendNodes = legends.map(legend => (
+            <span key={legend.id} className="pie_legends_item">
+                <span className="pie_legends_item_color" style={{ background: legend.color }}/>
+                <span className="pie_legends_item_count">{legend.count}</span>
+                {legend.label}
+            </span>
+        ));
+
+        return (
+            <div className="pie_legends">
+                {legendNodes}
+            </div>
+        );
+
+    }
+}
+
+PieLegends.propTypes = {
+    legends: PropTypes.array.isRequired
+};
+
+
+export default PieLegends;
