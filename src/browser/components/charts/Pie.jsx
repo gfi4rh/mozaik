f1e22e789a31fcfe2be1d358488890b2c2d101f2
colin27091 <colinregourd@gmail.com>
2021-01-14 10:11:42 +0100

    montée en version react

--- src/browser/components/charts/Pie.jsx
+++ src/browser/components/charts/Pie.jsx
@@ -1,4 +1,5 @@
-import React, { Component, PropTypes } from 'react';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import PieLegends                      from './PieLegends.jsx';
 import PieCount                        from './PieCount.jsx';
 import PieChart                        from './PieChart.jsx';
