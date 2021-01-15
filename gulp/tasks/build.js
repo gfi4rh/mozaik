4307a8e7a5632319ed293cc46ea1ed7593a76bb7
colin27091 <colinregourd@gmail.com>
2021-01-15 14:26:50 +0100

    restore gulp build

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,9 +1,5 @@
 var gulp = require('gulp');
 
 
-//gulp.task('build', gulp.series('copy', 'styles', 'js'));
+gulp.task('build', gulp.series('copy', 'styles', 'js'));
 
-gulp.task('build', () => {
-  console.log('je build')
-  return Promise.resolve('build')
-})
