7f439322cc77e8b4dd8ebf31d44861a4f8a2ad61
colin27091 <colinregourd@gmail.com>
2021-01-15 15:10:10 +0100

    debug on gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,5 @@
 var gulp = require('gulp');
 
-require(require('path').resolve(__dirname + '/gulp'));
-
+var stock = require(require('path').resolve(__dirname + '/gulp'));
+console.log(stock)
 module.exports = gulp;
