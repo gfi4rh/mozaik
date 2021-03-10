80e8467e8e0158236af5798e9778bbbe27ff511f
Regourd Colin <colin.regourd@inetum.world>
2021-03-10 11:16:18 +0100

    change on bus

--- src/Bus.js
+++ src/Bus.js
@@ -63,7 +63,7 @@ const Bus = mozaik => {
         apis[id] = { methods: api(mozaik), mode };
 
         mozaik.logger.info(JSON.stringify(apis) + ' ' +id);
-        mozaik.logger.info(chalk.yellow(`registered API '${id}' (mode: ${mode})`));
+        mozaik.logger.info(chalk.yellow(`Registered API '${id}' (mode: ${mode})`));
     };
 
     /**
