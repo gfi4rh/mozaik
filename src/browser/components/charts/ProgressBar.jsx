1fb9800187e7fd83a80ed3193b3636c2d0411155
Regourd Colin <colin.regourd@inetum.world>
2021-03-10 17:17:53 +0100

    add proptype to progress bar

--- src/browser/components/charts/ProgressBar.jsx
+++ src/browser/components/charts/ProgressBar.jsx
@@ -1,5 +1,4 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
+import React, { Component, PropTypes } from 'react';
 
 
 class ProgressBar extends Component {
@@ -18,10 +17,11 @@ class ProgressBar extends Component {
           </div>
       );
   }
-}
+};
 
-/*ProgressBar.propTypes = {
-};*/
+ProgressBar.propTypes = {
+  completed: PropTypes.number.isRequired
+};
 
 ProgressBar.displayName = 'ProgressBar';
 
