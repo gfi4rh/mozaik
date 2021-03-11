3dd9285d1bee99c02189d41c328d1f557c243ce2
Regourd Colin <colin.regourd@inetum.world>
2021-03-11 09:56:03 +0100

    restore progress bar

--- src/browser/components/charts/ProgressBar.jsx
+++ src/browser/components/charts/ProgressBar.jsx
@@ -4,48 +4,36 @@ import PropTypes from 'prop-types';
 
 class ProgressBar extends Component {
 
-  componentDidMount() {
-
-  }
-
-  shouldComponentUpdate(){
-    
-  }
-
-
   render() {
 
-    /*const { completed, color, animation, height } = this.props;
+    const { completed, color, animation, height } = this.props;
 
     const style = {
       backgroundColor: color,
       width: completed + '%',
       transition: `width ${animation}ms`,
       height: height,
-    };*/
-
-    // style={style}
+    };
 
     return (
-        <div className="progress-bar_wrapper">
-          Hello
+        <div className="progress-bar_wrapper" style={style}>
         </div>
     );
   }
 };
 
-/*ProgressBar.defaultProps = {
+ProgressBar.defaultProps = {
   completed: 10,
   color: '#0BD318',
   animation: 200,
   height: 10
-};*/
+};
 
 ProgressBar.propTypes = {
-  /*completed: PropTypes.number,
+  completed: PropTypes.number,
   color: PropTypes.string,
   animation: PropTypes.number,
-  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number])*/
+  height: PropTypes.oneOfType([PropTypes.string, PropTypes.number])
 };
 
 ProgressBar.displayName = 'ProgressBar';
