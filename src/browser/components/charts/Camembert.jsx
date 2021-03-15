5993ba2368f9ea58f09fc4f252a0c529c2ec5f0b
Regourd Colin <colin.regourd@inetum.world>
2021-03-15 11:17:43 +0100

    end pie

--- src/browser/components/charts/Camembert.jsx
+++ src/browser/components/charts/Camembert.jsx
@@ -12,28 +12,8 @@ class Camembert extends Component {
   }
 
   componentDidMount() {
-    const data = {
-      labels: [
-        'Red',
-        'Blue',
-        'Yellow'
-      ],
-      datasets: [{
-        data: [300, 50, 100],
-        backgroundColor: [
-        '#FF6384',
-        '#36A2EB',
-        '#FFCE56'
-        ],
-        hoverBackgroundColor: [
-        '#FF6384',
-        '#36A2EB',
-        '#FFCE56'
-        ]
-      }]
-    };
 
-    const {options, legend, type } = this.props;
+    const { data, options, legend, type } = this.props;
 
     options.legend = legend;
 
