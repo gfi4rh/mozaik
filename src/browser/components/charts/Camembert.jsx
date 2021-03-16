e66ee41dac69868d6e724546ef27594836ca2ee7
Regourd Colin <colin.regourd@inetum.world>
2021-03-16 10:42:46 +0100

    change size legend

--- src/browser/components/charts/Camembert.jsx
+++ src/browser/components/charts/Camembert.jsx
@@ -24,7 +24,7 @@ class Camembert extends Component {
 
     Chart.defaults.global.defaultFontColor = style.getPropertyValue('color');
     Chart.defaults.global.defaultFontFamily = style.getPropertyValue('font-family')
-    Chart.defaults.global.defaultFontSize = 15
+    Chart.defaults.global.defaultFontSize = '2vmin'
 
     this.chartInstance = new Chart(ctx, {
       type,
