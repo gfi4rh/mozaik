fcf1b9e1bcab5afa45421905752ed997a610f546
Regourd Colin <colin.regourd@inetum.world>
2021-05-10 11:44:55 +0200

    forward button

--- src/browser/index.js
+++ src/browser/index.js
@@ -1,16 +1,19 @@
 import ComponentRegistry from './component-registry';
 import ApiConsumerMixin  from './mixins/ApiConsumerMixin';
 import ApiStore          from './stores/ApiStore';
+import DashboardStore from './stores/DashboardStore';
 import ApiActions        from './actions/ApiActions';
+import DashboardActions from './actions/DashboardActions';
 import ConfigActions     from './actions/ConfigActions';
 import MozaikComponent   from './components/Mozaik.jsx';
 import PieComponent      from './components/charts/Pie.jsx';
 import GaugeComponent    from './components/charts/Gauge.jsx';
 import TreemapComponent  from './components/charts/Treemap.jsx';
 import BarChartComponent from './components/charts/BarChart.jsx';
-import ProgressBarComponent from './components/charts/ProgressBar.jsx'
-import GraphicComponent from './components/charts/Graphic.jsx'
+import ProgressBarComponent from './components/charts/ProgressBar.jsx';
+import GraphicComponent from './components/charts/Graphic.jsx';
 import Inspector         from './components/Inspector.jsx';
+
 
 
 ComponentRegistry.add('mozaik.inspector', Inspector);
@@ -22,11 +25,13 @@ export default {
         ApiConsumer: ApiConsumerMixin
     },
     Store: {
-        Api: ApiStore
+        Api: ApiStore,
+        Dashboard : DashboardStore
     },
     Actions: {
         Api:    ApiActions,
-        Config: ConfigActions
+        Config: ConfigActions,
+        Dashboard : DashboardActions
     },
     Component: {
         Mozaik:   MozaikComponent,
