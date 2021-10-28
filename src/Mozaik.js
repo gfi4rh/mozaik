c3cc2d5c9afd785a0d4d211b4e641212bf04ae39
Regourd Colin <colin.regourd@inetum.world>
2021-10-28 09:55:04 +0200

    put /logs directory to root

--- src/Mozaik.js
+++ src/Mozaik.js
@@ -26,7 +26,7 @@ class Mozaik {
                     )
                 }),
                 new winston.transports.File({
-                    filename : `./logs/info-${new Date().toISOString().replace(/\T.+/, '')}-0.log`,
+                    filename : `/logs/info-${new Date().toISOString().replace(/\T.+/, '')}-0.log`,
                     format : winston.format.combine(
                         winston.format.uncolorize(),
                         format
